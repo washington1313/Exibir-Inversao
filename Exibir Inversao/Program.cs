a471263736f687445dfcbfe47816f81bbcce0fc5
washington1313 <123412352+washington1313@users.noreply.github.com>
2023-04-13 21:40:01 -0300

    Add files via upload

---
+++ Exibir Inversao/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exibir_Inversao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomesEntrada = new string[5];
+            int totalNomes = nomesEntrada.Length;
+
+            for (int i = 0; i < nomesEntrada.Length; i++)
+            {
+                Console.WriteLine("Digite os nomes: ");
+                nomesEntrada[i] = Console.ReadLine();
+
+                Console.Clear();
+            }
+
+            for (int i = 0; i < totalNomes; i++)
+            {
+                Console.WriteLine("Nomes exibidos em ordem de cadastro: " + nomesEntrada[i]);
+            }
+
+            for (int i = totalNomes - 1; i >= 0; i--)
+            {
+                Console.WriteLine("Nomes exibidos em ordem contraria: " + nomesEntrada[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
